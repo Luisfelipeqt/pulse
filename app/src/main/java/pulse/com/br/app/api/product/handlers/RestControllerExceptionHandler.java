6d74bf9569f524c29ec6dc93db1b413c54121b65
felipe <luisfelipebr1995@gmail.com>
2024-10-03 02:56:22 -0300

    feat: Implement exception handler for ModelNotFound, ProductAlreadyExist, and UserAlreadyExist
    - Added @ExceptionHandler annotations for ModelNotFoundException, ProductAlreadyExistException, and UserAlreadyExistException, returning appropriate responses with HTTP status 404 and 409.
    - Implemented validation error handling logic in the handleMethodArgumentNotValid method, returning a ValidationErrorResponse object formatted in snake_case.
    - Improved error response formatting, including status, message, and timestamp.

--- app/src/main/java/pulse/com/br/app/api/product/handlers/RestControllerExceptionHandler.java
+++ app/src/main/java/pulse/com/br/app/api/product/handlers/RestControllerExceptionHandler.java
@@ -1,7 +1,6 @@
 package pulse.com.br.app.api.product.handlers;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
